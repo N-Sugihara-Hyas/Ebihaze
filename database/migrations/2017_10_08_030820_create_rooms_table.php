2c7983799ad9c339d082d2469c5ffd680856218a
Naoki Sugihara <sugihara.blossom@gmail.com>
2017-10-08 12:30:57 +0900

    Add, Migrate - Create Room Table

---
+++ database/migrations/2017_10_08_030820_create_rooms_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRoomsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->increments('id');
+	        $table->string('room_number');
+	        $table->string('floor')->nullable()->comment('所在階');
+	        $table->string('floor_plan')->nullable()->comment('間取り');
+	        $table->integer('building_id');
+	        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rooms');
+    }
+}
