4fe9dfb6e9c33eeb8e15bb7244950b683bf42287
Naoki Sugihara <sugihara.blossom@gmail.com>
2017-10-08 11:42:46 +0900

    Migrate - Create Users Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,25 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+
+	        $table->string('tel')->unique();
+	        $table->string('nickname');
+	        $table->string('gender');
+	        $table->string('birthday');
+	        $table->string('job');
+	        $table->string('fam');
+	        $table->string('pet');
+	        $table->boolean('certification')->comment('SMS認証');//!SMS認証
+	        $table->string('type')->comment('[app, officer, common / trader]');//[app, officer, common / trader] TODO::Trader?
+			$table->string('owned')->comment('[owner, rent, treader]');//[owner, rent, treader] TODO::Trader?
+			$table->string('reside')->comment('[residents, rentout, trader]');//[residents, rentout, trader]
+			$table->string('trader')->commnet('[none, collaborator, trader]');//[none, collaborator, trader]
+			$table->tinyInteger('approval')->coment('[0:temporary, 1:authorized, 9:expired]');//[temporary, authorized, expired]
+			$table->boolean('membership')->comment('[0: 無料, 1:有料]');//[0: 無料, 1:有料]
+	        $table->integer('apartment_id');
+	        $table->integer('building_id');
+	        $table->integer('room_id');
+
             $table->rememberToken();
             $table->timestamps();
         });
