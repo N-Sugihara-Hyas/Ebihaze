15d165df3795c25ae42def98f0f48c1e70624bcc
Naoki Sugihara <sugihara.blossom@gmail.com>
2017-10-29 11:12:03 +0900

    Migrate - Alter UsersTable Email Nullable

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,25 +16,25 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
-            $table->string('email')->unique();
-            $table->string('password');
+            $table->string('email')->unique()->nullable();
+            $table->string('password')->nullable();
 
 	        $table->string('tel')->unique();
-	        $table->string('nickname');
+	        $table->string('nickname')->nullable();
 	        $table->string('gender')->nullable();
 	        $table->string('birthday')->nullable();
 	        $table->string('job')->nullable();
 	        $table->string('fam')->nullable();
 	        $table->string('pet')->nullable();
-	        $table->boolean('certification')->comment('SMS認証');//!SMS認証
+	        $table->boolean('certification')->default(0)->comment('SMS認証');//!SMS認証
 	        $table->string('auth_token')->nullable();
-	        $table->boolean('notification')->comment('他者通知可否');
-	        $table->string('type')->comment('[app, officer, common / trader]');//[app, officer, common / trader] TODO::Trader?
-			$table->string('owned')->comment('[owner, rent, treader]');//[owner, rent, treader] TODO::Trader?
-			$table->string('reside')->comment('[residents, rentout, trader]');//[residents, rentout, trader]
-			$table->string('trader')->commnet('[none, collaborator, trader]');//[none, collaborator, trader]
-			$table->tinyInteger('approval')->coment('[0:temporary, 1:authorized, 9:expired]');//[temporary, authorized, expired]
-			$table->boolean('membership')->comment('[0: 無料, 1:有料]');//[0: 無料, 1:有料]
+	        $table->boolean('notification')->default(0)->comment('他者通知可否');
+	        $table->string('type')->nullable()->comment('[app, officer, common / trader]');//[app, officer, common / trader] TODO::Trader?
+			$table->string('owned')->nullable()->comment('[owner, rent, treader]');//[owner, rent, treader] TODO::Trader?
+			$table->string('reside')->nullable()->comment('[residents, rentout, trader]');//[residents, rentout, trader]
+			$table->string('trader')->nullable()->commnet('[none, collaborator, trader]');//[none, collaborator, trader]
+			$table->tinyInteger('approval')->default(0)->coment('[0:temporary, 1:authorized, 9:expired]');//[temporary, authorized, expired]
+			$table->boolean('membership')->default(0)->comment('[0: 無料, 1:有料]');//[0: 無料, 1:有料]
 	        $table->integer('apartment_id')->nullable();
 	        $table->integer('building_id')->nullable();
 	        $table->integer('room_id')->nullable();
---
+++ resources/views/users/certificate.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.globalmenu')
+
+@section('content')
+<div class="container">
+    <form action="{{route('post.users-create')}}" method="post">
+        <div class="users-invite_form">
+            <div class="users-invite_form__introduction">
+                <p class="users-invite_form__text">
+                    ユーザー登録を行います<br>
+                    SMSの番号を<br>
+                    入力してください<br>
+                    送信後SMSが届きますので<br>
+                    SMSに記載のIDを入力の上<br>
+                    ユーザー情報入力にお進み下さい<br>
+                </p>
+            </div>
+            <div class="users-invite_form__form">
+                <input name="user_tel" type="text" maxlength="13">
+            </div>
+            <div class="users-invite_form__submit">
+                <button class="c-btn c-btn--large c-btn--blue action" data-method="post">送信</button>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
