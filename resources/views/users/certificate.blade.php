c31580228e46ff2975068068cbca15e7c924536b
Naoki Sugihara <sugihara.blossom@gmail.com>
2017-10-29 11:26:23 +0900

    Add - UsersController@certificate Function

--- resources/views/users/certificate.blade.php
+++ resources/views/users/certificate.blade.php
@@ -2,20 +2,21 @@
 
 @section('content')
 <div class="container">
-    <form action="{{route('post.users-create')}}" method="post">
+    <form action="{{route('post.users-certificate')}}" method="post">
+        {{ csrf_field() }}
         <div class="users-invite_form">
             <div class="users-invite_form__introduction">
                 <p class="users-invite_form__text">
-                    ユーザー登録を行います<br>
-                    SMSの番号を<br>
-                    入力してください<br>
-                    送信後SMSが届きますので<br>
-                    SMSに記載のIDを入力の上<br>
-                    ユーザー情報入力にお進み下さい<br>
+                    SMSに送信されたコードの<br>
+                    入力をお願いします。<br>
+                    SMSで発行されたコードの<br>
+                    有効期限は24時間です<br>
                 </p>
             </div>
             <div class="users-invite_form__form">
-                <input name="user_tel" type="text" maxlength="13">
+                <input name="user_auth_token" type="text" maxlength="13">
+                <input name="user_id" type="hidden" value="{{$user->id}}">
+                <input name="user_type" type="hidden" value="officer">
             </div>
             <div class="users-invite_form__submit">
                 <button class="c-btn c-btn--large c-btn--blue action" data-method="post">送信</button>
