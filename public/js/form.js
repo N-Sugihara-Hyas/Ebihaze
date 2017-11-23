685506042877acef115916b6737aa2bfc743f987
Naoki Sugihara <sugihara.blossom@gmail.com>
2017-11-23 22:35:29 +0900

    FB-Add - Image in Comments

--- public/js/form.js
+++ public/js/form.js
@@ -5,6 +5,9 @@ console.log($(this).data('method'));
             var $actioned = $(this);
             switch($(this).data('method')){
                 case 'post':
+                    if($('#required').length >= 1){
+                        if($('#required').val()=='')return false;
+                    }
                     $('form').submit();
                     return false;
                     break;
@@ -21,6 +24,26 @@ console.log('click');
                         };
                         //ファイル名を表示
                         $(this).parent().find('.filename').html('ファイル名：' + file.name);
+                    });
+                    return false;
+                    break;
+                case 'file-comment':
+                    $(this).parent().find('input[type="file"]').trigger('click');
+                    $(this).parent().find('input[type="file"]').on('change', function(){
+                        var file = $(this).prop('files')[0];
+console.log(file);
+                        //アイコンを選択中に変更
+                        if(file===undefined){
+                            $actioned.removeClass('c-btn--select').html('+');
+                        }else{
+                            $actioned.addClass('c-btn--select').html('-');
+                        }
+                        //未選択→選択の場合（.filenameが存在しない場合）はファイル名表示用の<div>タグを追加
+                        // if(!($(this).parent().find('.filename').length)){
+                        //     $actioned.parent().append('<div class="filename"></div>');
+                        // };
+                        //ファイル名を表示
+                        // $(this).parent().find('.filename').html('ファイル名：' + file.name);
                     });
                     return false;
                     break;
--- resources/views/events/message.blade.php
+++ resources/views/events/message.blade.php
@@ -7,10 +7,15 @@
     <input type="hidden" name="comment_commentable_type" value="App\Event">
     <ul class="event-message c-list">
         @foreach($event->comments as $comment)
-        @if($comment->user_id!=1)
+        @if($comment->user_id!=Auth::id())
         <li class="event-message__item event-message__item--you">
             <div class="event-message-container">
                 <section class="event-message__body--you c-balloon--you">
+                    @if($comment->is_image==1)
+                        <figure>
+                            <img src="{{asset('img/resources/comment/'.$comment->id.'/image')}}" alt="">
+                        </figure>
+                    @endif
                     <span>{{$comment->body}}</span>
                     <div class="c-balloon--container">
                         <div class="c-balloon__parts--you">&nbsp;</div>
@@ -18,7 +23,7 @@
                 </section>
                 <section class="event-message__thumb">
                     <figure>
-                        <img src="" alt="">
+                        <img class="c-circle" src="{{asset('img/resources/user/'.$comment->user_id.'/icon')}}" alt="">
                     </figure>
                 </section>
             </div>
@@ -26,6 +31,11 @@
         @else
         <li class="event-message__item event-message__item--me">
             <section class="event-message__body--me c-balloon--me">
+                @if($comment->is_image==1)
+                    <figure>
+                        <img src="{{asset('img/resources/comment/'.$comment->id.'/image')}}" alt="">
+                    </figure>
+                @endif
                 <span>{{$comment->body}}</span>
                 <div class="c-balloon--container">
                     <div class="c-balloon__parts--me">&nbsp;</div>
